f296fc70274d7264a43e56f9d5c7bf4d48ffffa9
quangngonz <46818455+quangngonz@users.noreply.github.com>
2022-02-19 18:08:02 +0700

    upload Build

---
+++ build/precache-manifest.a663526607faff6254999c0418c0f91d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e3aa1056b12d4d5d10463f81d1081f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83ea7b39a785109bc3b8",
+    "url": "/static/css/2.75ee30cf.chunk.css"
+  },
+  {
+    "revision": "cb5f0afd55dfa9518851",
+    "url": "/static/css/main.9d04bd59.chunk.css"
+  },
+  {
+    "revision": "83ea7b39a785109bc3b8",
+    "url": "/static/js/2.21e12cc3.chunk.js"
+  },
+  {
+    "revision": "739d75e7b081c99083237b1b73d68faf",
+    "url": "/static/js/2.21e12cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5f0afd55dfa9518851",
+    "url": "/static/js/main.2a0d8f78.chunk.js"
+  },
+  {
+    "revision": "4400c55e67822066533f",
+    "url": "/static/js/runtime-main.cbe74a98.js"
+  },
+  {
+    "revision": "56c038c94c6f2ecec8ba07b3bbaf37d9",
+    "url": "/static/media/Logo.56c038c9.png"
+  }
+]);
